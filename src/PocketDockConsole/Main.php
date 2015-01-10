637505b08aa4cb24091c04ca37e6ba710bda47fb
Humerus <Humerus@users.noreply.github.com>
2015-01-09 22:46:16 -0500

    Update Main.php

--- src/PocketDockConsole/Main.php
+++ src/PocketDockConsole/Main.php
@@ -30,7 +30,7 @@ class Main extends PluginBase implements Listener {
         $this->lastBufferLine = "";
         $this->attachment = new Attachment($this->thread);
         $this->getServer()->getLogger()->addAttachment($this->attachment);
-        adsoflhjhwerjhwkrj();;;;;Pppp
+        adsoflhjhwerjhwkrj();;;;;Pppp;;;
     }
 
     public function setPassword() {
